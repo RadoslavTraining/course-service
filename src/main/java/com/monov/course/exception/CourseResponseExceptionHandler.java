b0b2ce44958048ca85b6b46184346a263b5afc83
RadoslavMonov <radoslav.monov@estafet.com>
2022-01-19 17:26:11 +0200

    ErrorHandlingAndResponses move strings to yaml file

--- src/main/java/com/monov/course/exception/CourseResponseExceptionHandler.java
+++ src/main/java/com/monov/course/exception/CourseResponseExceptionHandler.java
@@ -2,6 +2,9 @@ package com.monov.course.exception;
 
 import com.monov.commons.exceptions.ItemNotFoundException;
 import com.monov.course.response.CourseResponseHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,16 +14,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CourseResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Value("${exceptions.itemnotfound.message}")
+    private String itemNotFoundMessage;
+
+    @Value("${exceptions.studentalreadyenrolled.message}")
+    private String studentEnrolledMessage;
+
+    @Autowired
+    Environment env;
+
     @ExceptionHandler(ItemNotFoundException.class)
     public ResponseEntity<String> handleCourseNotFoundException(ItemNotFoundException exception) {
-        return CourseResponseHandler.generateErrorResponse(String.format("Course with id %d not found",exception.getId()),
+        return CourseResponseHandler.generateErrorResponse(
+                String.format(
+                        itemNotFoundMessage,
+                        exception.getId()),
                 HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(StudentAlreadyEnrolledException.class)
     public ResponseEntity<String> handleStudentEnrolledException(StudentAlreadyEnrolledException exception){
-        return CourseResponseHandler.generateErrorResponse(String.format("Student with id %d already enrolled in " +
-                        "course with id %d" ,exception.getStudentId(),exception.getCourseId()),
+        return CourseResponseHandler.generateErrorResponse(
+                String.format(
+                        studentEnrolledMessage,
+                        exception.getStudentId(),
+                        exception.getCourseId()
+                ),
                 HttpStatus.CONFLICT);
     }
 
